149cdd21e12e7ca7c5429e0f3765f3cde9f3aad8
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-10-22 20:25:14 -0700

    feat(infra): add remote backend identifier

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,8 +1,21 @@
 terraform {
   required_version = ">=0.12"
+
+  required_providers {
+    aws = ">=2.32"
+  }
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "ajtorres9"
+
+    workspaces {
+      name = "andrewjtorr-es"
+    }
+  }
 }
 
 provider "aws" {
-  version    = ">=2.32"
-  region     = "us-east-1"
+  version = ">=2.32"
+  region  = "us-east-1"
 }
