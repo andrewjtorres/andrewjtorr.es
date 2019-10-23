4e42bf9b203948eb3c26afb3f676498388f35f86
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-10-22 20:27:48 -0700

    chore(providers): bump aws from 2.32 to 2.33

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12"
 
   required_providers {
-    aws = ">=2.32"
+    aws = ">=2.33"
   }
 
   backend "remote" {
@@ -16,6 +16,6 @@ terraform {
 }
 
 provider "aws" {
-  version = ">=2.32"
+  version = ">=2.33"
   region  = "us-east-1"
 }
