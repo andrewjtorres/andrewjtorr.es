8841ca8c302e7c5321f4ec3fdfdabe249512e0a7
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-12-02 13:05:42 -0800

    update

--- scripts/start.ts
+++ scripts/start.ts
@@ -65,7 +65,7 @@ const createCompilationPromise = (
     })
   })
 
-const noop = () => {}
+const noop = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
 
 const start = async () => {
   if (server) {
