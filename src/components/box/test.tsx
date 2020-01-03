955f45a24fc7d37dcbe6a289e1becd55adac5f8a
Andrew Torres <andrew.jonathan.torres@gmail.com>
2020-01-02 17:13:47 -0800

    test: update box props used for inline snapshot

--- src/components/box/test.tsx
+++ src/components/box/test.tsx
@@ -4,41 +4,17 @@ import { renderWithContext } from 'utils/spec'
 import { Box } from '.'
 
 test('should render correctly', () => {
-  const { firstChild } = renderWithContext(
-    <Box bg="primary" color="white" fontSize={4} p={5} width={[1, 1, 0.5]}>
-      Box
-    </Box>
-  ).container
+  const { firstChild } = renderWithContext(<Box>Box</Box>).container
 
   expect(firstChild).toMatchInlineSnapshot(`
     .c0 {
       box-sizing: border-box;
       margin: 0;
       min-width: 0;
-      background-color: #f15b5b;
-      color: #fff;
-      font-size: 1.5rem;
-      padding: 4rem;
-      width: 100%;
-    }
-
-    @media screen and (min-width:769px) {
-      .c0 {
-        width: 100%;
-      }
-    }
-
-    @media screen and (min-width:1024px) {
-      .c0 {
-        width: 50%;
-      }
     }
 
     <div
       class="c0"
-      color="white"
-      font-size="4"
-      width="1,1,0.5"
     >
       Box
     </div>
