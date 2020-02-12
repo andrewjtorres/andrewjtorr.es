0fd2aa92e947f570d46a5ce6009492c7b296fd82
Andrew Torres <andrew.jonathan.torres@gmail.com>
2020-02-11 20:05:40 -0800

    chore(infra): bump aws from 2.46 to 2.48

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12"
 
   required_providers {
-    aws = ">=2.46"
+    aws = ">=2.48"
   }
 
   backend "remote" {
@@ -22,7 +22,7 @@ locals {
 }
 
 provider "aws" {
-  version = ">=2.46"
+  version = ">=2.48"
   region  = "us-east-1"
 }
 
