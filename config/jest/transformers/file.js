91e5bea7ffc9f390f03e5871dc0ed4a62c1cbf1d
Andrew Torres <andrew.jonathan.torres@gmail.com>
2020-11-02 13:52:52 -0800

    fix: address improper code sanitization

--- config/jest/transformers/file.js
+++ config/jest/transformers/file.js
@@ -1,13 +1,17 @@
 'use strict'
 
 const path = require('path')
+const camelCase = require('camelcase')
+const serialize = require('serialize-javascript')
 
 module.exports = {
   process: (_sourceText, sourcePath) => {
-    const filename = JSON.stringify(path.basename(sourcePath))
+    const { base, name } = path.parse(sourcePath)
+    const fileBase = serialize(base)
+    const fileName = camelCase(name, { pascalCase: true })
 
     return /\.svg$/.test(sourcePath)
-      ? `module.exports = { __esModule: true, default: (props) => ({ $$typeof: Symbol.for('react.element'), key: null, props: Object.assign({}, props, { children: ${filename} }), ref: null, type: 'svg' }) };`
-      : `module.exports = ${filename};`
+      ? `const React=require('react');module.exports={__esModule:true,default:React.forwardRef(function Svg${fileName}(props,ref){return {$$typeof:Symbol.for('react.element'),key:null,props:Object.assign({},props,{children:${fileBase}}),ref,type:'svg'}})};`
+      : `module.exports=${fileBase};`
   },
 }
