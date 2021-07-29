029e087bf53f521a74726d9d45abddf99b9728f0
Andrew Torres <andrew.jonathan.torres@gmail.com>
2021-07-29 15:45:44 -0700

    chore(deps): bump @graphql-tools/schema from 7.1.5 to 8.0.1

--- src/api/schema.ts
+++ src/api/schema.ts
@@ -3,7 +3,6 @@ import { makeExecutableSchema } from '@graphql-tools/schema'
 import * as translation from './translation'
 
 export default makeExecutableSchema({
-  logger: __IS_DEV__ ? { log: (error) => console.error(error) } : undefined,
   resolvers: [translation.resolvers],
   typeDefs: [translation.typeDefs],
 })
